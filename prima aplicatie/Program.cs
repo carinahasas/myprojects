280fc8531fd70967f9fd61da95df192454aabb4a
Carina Hasas <hasas.carina@yahoo.com>
2022-11-21 15:03:10 +0200

    add project 21.11.22

--- prima aplicatie/Program.cs
+++ prima aplicatie/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(5+5);
+
+string[] telefoane = { "Samsung", "Iphone", "Allview", "Motorolla" };
+foreach(string telefon in telefoane)
+{
+    Console.WriteLine(telefon);
+}
